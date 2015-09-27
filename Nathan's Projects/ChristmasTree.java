054b141074031253aae953a8b57fa04426e692b1
natechaufan88 <natechaufan88@gmail.com>
2015-09-27 15:59:21 -0700

    Christmas Tree

---
+++ Nathan's Projects/ChristmasTree.java
@@ -0,0 +1,36 @@
+/* Nathan Chau
+ * September 28, 2015
+ * This program will use parameters to print out any size Christmas Tree.
+ */
+
+public class ChristmasTree {
+    public static void drawTree(int max_segments, int max_height) {
+        for (int segment = 1; segment <= max_segments; segment++) {
+            for (int line = 1; line <= max_height; line++) {
+                for (int space = 1; space <= (7 - segment) - line; space++) {
+                    System.out.print(" ");
+                }
+                
+                for (int star = 1; star <= (2 * line) + (2 * segment) - 3; star++) {
+                    System.out.print("*");
+                }
+                
+                System.out.println();
+            }
+        }
+    }
+    
+    public static void bottom() {
+        System.out.println("     *");
+        System.out.println("     *");
+        System.out.println("  *******");
+    }
+    
+    public static void main(String[] args) {
+        drawTree(3, 4);
+        bottom();
+        System.out.println();
+        drawTree(2, 5);
+        bottom();
+    }
+}
